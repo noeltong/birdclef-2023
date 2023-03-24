05ec2c996ef645bc2a3678946ae0e5b9adbe6da2
noeltong <2441624985@qq.com>
2023-03-24 21:42:38 +0800

    improve data pipeline

--- utils/data.py
+++ utils/data.py
@@ -6,21 +6,44 @@ from utils import audio
 
 
 class BirdCLEFDataset(Dataset):
-    def __init__(self, data_lst):
+    def __init__(self, data_lst, mode, sample_rate=32000, resample_rate=16000, num_channels=3, duration=8000):
         super().__init__()
         self.data = data_lst
+        self.sample_rate = sample_rate
+        self.resample_rate = resample_rate
+        self.channel = num_channels
+        self.duration = duration
+        self.mode = mode
 
     def __getitem__(self, idx):
         path, label = self.data[idx]
         aud = audio.read_file(path)
 
-        return aud, label
+        aud = audio.resample(aud, self.resample_rate)
+        aud = audio.rechannel(aud, self.channel)
+        aud = audio.pad_trunc(aud, self.resample_rate, self.duration)
+
+        if self.mode == 'train':
+            sg1 = self.sg_augment(aud)
+            sg2 = self.sg_augment(aud)
+
+            return sg1, sg2
+        
+        elif self.mode == 'tune':
+            sg = self.sg_augment(aud)
+
+            return sg, label
     
     def __len__(self):
         return len(self.data)
     
+    def sg_augment(self, aud):
+        aud = audio.time_shift(aud, self.shift_factor)
+        sg = audio.spectrogram(aud, n_mels=self.n_mels, n_fft=self.n_fft, hop_len=None)
+        sg = audio.spectro_augment(sg, max_mask_pct=0.1, n_freq_masks=self.n_masks, n_time_masks=self.n_masks)
+    
 
-def get_dataloader(config):
+def get_dataloader(config, mode):
 
     csv_path = config.data.csv_path
 
@@ -39,8 +62,8 @@ def get_dataloader(config):
     idx = int(0.9 * len(data))
     train_data, test_data = data[:idx], data[idx:]
 
-    train_dataset = BirdCLEFDataset(train_data)
-    test_dataset = BirdCLEFDataset(test_data)
+    train_dataset = BirdCLEFDataset(train_data, mode)
+    test_dataset = BirdCLEFDataset(test_data, mode)
 
     train_sampler = DistributedSampler(train_dataset)
     test_sampler = DistributedSampler(test_dataset)
