00cd354b4980605584af808c853c8312d6462d1f
noeltong <2441624985@qq.com>
2023-03-24 16:22:06 +0800

    add config

--- configs/default_config.py
+++ configs/default_config.py
@@ -9,4 +9,26 @@ def default_config():
     pretrain.num_epochs = 500
 
 
+    # ----------------
+    # Optimization
+    # ----------------
+
+    cfg.optim = optim = ConfigDict()
+    optim.optimizer = 'LAMB'
+    optim.schedule = 'CosineAnnealingLR'
+    optim.grad_clip = 1.
+    optim.initial_lr = 2.5e-4
+    optim.weight_decay = 0.1
+    optim.min_lr = 0.001 * optim.initial_lr
+    optim.warmup_epochs = 25
+
+
+    data = cfg.data = ConfigDict()
+    data.csv_path = '/public/home/tongshq/kaggle/birdclef/model_data/data_all.csv'
+    data.num_workers = 4
+
+
+    cfg.seed = 42
+
+
     return cfg
