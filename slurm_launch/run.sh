fc354dbc9659f670ffade79ffbe543a7463bc17a
noeltong <2441624985@qq.com>
2023-03-25 14:53:10 +0800

    update

--- slurm_launch/run.sh
+++ slurm_launch/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=ft_rm_b
+#SBATCH --job-name=pretrain
 #SBATCH --partition=normal
 #SBATCH -N 1
 #SBATCH -n 4
@@ -12,6 +12,6 @@
 #SBATCH --output=logs/%j.out
 #SBATCH --error=logs/%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --exclude=sist_gpu[38,63-66]
+#SBATCH --nodelist=sist_gpu60
 
 torchrun --nproc_per_node=4 main.py --mode train --config configs/default_config.py --workdir test
