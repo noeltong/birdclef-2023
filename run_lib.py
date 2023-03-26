032289874aba47dff77523665bdb905175140fb5
noeltong <2441624985@qq.com>
2023-03-26 22:54:53 +0800

    move code

--- run_lib.py
+++ run_lib.py
@@ -12,6 +12,7 @@ from models.lars import LARS
 from models.convnext import ConvNeXt
 from utils.utils import seed_everything
 from models.builder import SimSiam
+from utils.utils import get_optim
 
 
 def train(config, workdir):
@@ -114,58 +115,10 @@ def train(config, workdir):
     # define optimization
     # -------------------
 
-    init_lr = config.optim.initial_lr * config.training.batch_size / 256
-
     if rank == 0:
         logger.info('Handling optimizations...')
 
-    if config.optim.optimizer.lower() == 'adamw':
-        optimizer = torch.optim.AdamW(
-            model.parameters(),
-            lr=init_lr,
-            weight_decay=config.optim.weight_decay,
-        )
-    elif config.optim.optimizer.lower() == 'adam':
-        optimizer = torch.optim.Adam(
-            model.parameters(),
-            lr=init_lr,
-            weight_decay=config.optim.weight_decay
-        )
-    elif config.optim.optimizer.lower() == 'lars':
-        optimizer = LARS(
-            model.parameters(),
-            lr=init_lr,
-            weight_decay=config.optim.weight_decay,
-        )
-    else:
-        raise NotImplementedError(
-            f'{config.optim.optimizer} is not supported.')
-
-    if config.optim.schedule.lower() is not None and config.optim.schedule.lower() == 'cosineannealinglr':
-        if config.optim.warmup_epochs is None:
-            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-                optimizer,
-                T_max=config.training.num_epochs,
-                eta_min=config.optim.min_lr,
-            )
-        else:
-            warmup_scheduler = torch.optim.lr_scheduler.LinearLR(
-                optimizer,
-                start_factor=config.optim.initial_lr / config.optim.warmup_epochs,
-                total_iters=config.optim.warmup_epochs
-            )
-            main_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-                optimizer,
-                T_max=config.training.num_epochs - config.optim.warmup_epochs,
-                eta_min=config.optim.min_lr
-            )
-            scheduler = torch.optim.lr_scheduler.SequentialLR(
-                optimizer,
-                schedulers=[warmup_scheduler, main_scheduler],
-                milestones=[config.optim.warmup_epochs]
-            )
-    else:
-        raise ValueError(f'{config.optim.schedule} is not supported.')
+    optimizer, scheduler = get_optim(model, config)
     
     criterion = nn.CosineSimilarity(dim=1).cuda()
 
--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,7 @@ import random
 import os
 import numpy as np
 import torch
+from models.lars import LARS
 
 
 def seed_everything(seed):
@@ -10,3 +11,57 @@ def seed_everything(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+
+
+def get_optim(model, config):
+    init_lr = config.optim.initial_lr * config.training.batch_size / 256
+
+    if config.optim.optimizer.lower() == 'adamw':
+        optimizer = torch.optim.AdamW(
+            model.parameters(),
+            lr=init_lr,
+            weight_decay=config.optim.weight_decay,
+        )
+    elif config.optim.optimizer.lower() == 'adam':
+        optimizer = torch.optim.Adam(
+            model.parameters(),
+            lr=init_lr,
+            weight_decay=config.optim.weight_decay
+        )
+    elif config.optim.optimizer.lower() == 'lars':
+        optimizer = LARS(
+            model.parameters(),
+            lr=init_lr,
+            weight_decay=config.optim.weight_decay,
+        )
+    else:
+        raise NotImplementedError(
+            f'{config.optim.optimizer} is not supported.')
+
+    if config.optim.schedule.lower() is not None and config.optim.schedule.lower() == 'cosineannealinglr':
+        if config.optim.warmup_epochs is None:
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+                optimizer,
+                T_max=config.training.num_epochs,
+                eta_min=config.optim.min_lr,
+            )
+        else:
+            warmup_scheduler = torch.optim.lr_scheduler.LinearLR(
+                optimizer,
+                start_factor=config.optim.initial_lr / config.optim.warmup_epochs,
+                total_iters=config.optim.warmup_epochs
+            )
+            main_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+                optimizer,
+                T_max=config.training.num_epochs - config.optim.warmup_epochs,
+                eta_min=config.optim.min_lr
+            )
+            scheduler = torch.optim.lr_scheduler.SequentialLR(
+                optimizer,
+                schedulers=[warmup_scheduler, main_scheduler],
+                milestones=[config.optim.warmup_epochs]
+            )
+    else:
+        raise ValueError(f'{config.optim.schedule} is not supported.')
+    
+    return optimizer, scheduler
